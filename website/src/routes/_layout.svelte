bbfa3874bd585b9b7b652732075af3dafa6dd88d
Patrik Thorsson <patrik@thorsson.io>
2021-01-16 22:42:09 +0100

    Minor clean up in _layout

--- website/src/routes/_layout.svelte
+++ website/src/routes/_layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import Nav from '../components/Nav.svelte';
-  // export let segment: string;
 </script>
 
 <div class="root-container">
@@ -20,9 +19,7 @@
   main {
     position: relative;
     max-width: 56em;
-    background-color: white;
     padding: 2em;
     margin: 0 auto;
-    box-sizing: border-box;
   }
 </style>
