19d563814afa7a218af5693e21d641b6de1a1403
Patrik Thorsson <patrik@thorsson.io>
2021-01-16 22:41:26 +0100

    Added more global types

--- website/src/global.d.ts
+++ website/src/global.d.ts
@@ -1,7 +1,47 @@
 declare module Website {
+  export type Settings = {
+    baseTitle: string;
+    baseUrl: string;
+    defaultMetaData: MetaData;
+  };
+
+  export type MetaData = {
+    title: string;
+    keywords: string;
+    description: string;
+    ogTitle: string;
+    ogDescription: string;
+    ogImage: ContentTypes.Image;
+  };
+
   export type Page = {
+    metaData: MetaData;
     slug: string;
     heading: string;
     body: string;
+  };
+}
+
+declare module ContentTypes {
+  export type Image = {
+    src: string;
+    alt: string;
+    width?: number;
+    height?: number;
+    mimeType?:
+      | 'image/gif'
+      | 'image/apng'
+      | 'image/flif'
+      | 'image/webp'
+      | 'image/x-mng'
+      | 'image/jpeg'
+      | 'image/png;';
+  };
+
+  export type Link = {
+    href: string;
+    text?: string;
+    description?: string;
+    target?: string;
   };
 }
