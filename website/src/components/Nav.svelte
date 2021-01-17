bf79b00110345709aabb12a4a2d3f91393eddd1e
Patrik Thorsson <patrik@thorsson.io>
2021-01-17 22:07:05 +0100

    Aligned nav with design Added support for SCSS

--- website/src/components/Nav.svelte
+++ website/src/components/Nav.svelte
@@ -1,31 +1,153 @@
 <script lang="ts">
   import { navItems } from '@data-file';
+
+  $: isOpen = false;
+
+  function toggle(open: boolean) {
+    isOpen = open;
+  }
 </script>
 
 <nav>
-  <ul>
+  {#if !isOpen}
+    <button class="open-menu" on:click={() => toggle(true)} />
+  {/if}
+  <ul class={isOpen ? 'is-open' : ''}>
     {#each navItems as link}
       {#if link.href && link.text}
         <li>
-          <a class="nav-link" rel="prefetch" href={link.href}>
+          <a rel="prefetch" href={link.href} on:click={() => toggle(false)}>
             {link.text}
           </a>
         </li>
       {/if}
     {/each}
   </ul>
+  {#if isOpen}
+    <button class="close-menu" on:click={() => toggle(false)}>Close</button>
+  {/if}
 </nav>
 
-<style>
+<style type="text/scss">
   nav {
+    display: flex;
+    flex-direction: column;
     position: absolute;
+    align-items: flex-end;
     top: 0;
     right: 0;
     z-index: 9999;
-    padding: 30px;
+    padding: calc(var(--base-margin) * 2);
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0;
+    margin: 0;
+    padding: 10px;
+    height: calc(19px + (var(--base-margin) * 2));
+    width: calc(26px + (var(--base-margin) * 2));
+    cursor: pointer;
+    transition: opacity 200ms;
+    box-sizing: border-box;
+    pointer-events: none;
+
+    &.is-open {
+      cursor: default;
+      height: auto;
+      width: auto;
+      pointer-events: initial;
+
+      li::after {
+        width: 100%;
+        opacity: 1;
+      }
+
+      a {
+        height: 26px;
+        opacity: 1;
+      }
+    }
   }
 
   li {
-    border: var(--border-width) solid var(--color-fg);
+    display: block;
+    position: relative;
+    margin-top: 5px;
+    transition: padding-right 200ms, opacity 200ms;
+    border-bottom: var(--border-width) solid transparent;
+
+    &:first-child {
+      margin-top: 0;
+    }
+
+    &:nth-child(n + 4)::after {
+      opacity: 0;
+    }
+
+    &::after {
+      position: absolute;
+      content: '';
+      width: 26px;
+      right: 0;
+      bottom: -3px;
+      border-bottom: var(--border-width) solid var(--color-fg);
+      transition: width 400ms, opacity 400ms;
+    }
+  }
+
+  a {
+    display: block;
+    position: relative;
+    font-size: 18px;
+    line-height: 26px;
+    height: 0px;
+    opacity: 0;
+    text-decoration: none;
+    overflow: hidden;
+    transition: height 400ms, opacity 400ms, padding-right 200ms;
+    flex: 0 0 auto;
+    color: var(--color-fg);
+    white-space: nowrap;
+
+    &:hover {
+      padding-right: 5px;
+    }
+  }
+
+  .close-menu {
+    position: relative;
+    outline: 0;
+    border: 0;
+    padding: 0;
+    margin: var(--base-margin);
+    background: transparent;
+    color: var(--color-fg);
+    transition: opacity 200ms;
+    cursor: pointer;
+    font-size: 12px;
+    opacity: 0.5;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  .open-menu {
+    height: calc(19px + (var(--base-margin) * 2));
+    width: calc(26px + (var(--base-margin) * 2));
+    border: 0;
+    padding: 0;
+    outline: 0;
+    background: transparent;
+    position: absolute;
+    z-index: 99;
+    cursor: pointer;
+
+    &:hover ~ ul {
+      opacity: 0.5;
+    }
   }
 </style>
