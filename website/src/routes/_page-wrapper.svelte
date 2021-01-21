144be0d975646eee42dd27cc34a93a0e09236bd2
Patrik Thorsson <patrik@thorsson.io>
2021-01-21 23:13:06 +0100

    Fixes to page wrapper

--- website/src/routes/_page-wrapper.svelte
+++ website/src/routes/_page-wrapper.svelte
@@ -6,17 +6,31 @@
 
 <MetaTags pageMetaData={page.metaData} />
 
-{#if page.heading}
-  <h1>{page.heading}</h1>
-{/if}
+<div class="inner-small">
+  {#if page.heading}
+    <h1>{page.heading}</h1>
+  {/if}
 
-{#if page.slug}
-  <small>{page.slug}</small>
-{/if}
+  {#if page.slug}
+    <small>{page.slug}</small>
+  {/if}
 
-{#if page.body}
-  <p>{@html page.body}</p>
-{/if}
+  {#if page.intro}
+    <p class="intro">{page.intro}</p>
+  {/if}
+
+  {#if page.body}
+    <p>{@html page.body}</p>
+  {/if}
+</div>
 
 <style>
+  .inner-small {
+    max-width: max(650px, 50vw);
+    background: green;
+  }
+
+  .intro {
+    font-size: 2em;
+  }
 </style>
